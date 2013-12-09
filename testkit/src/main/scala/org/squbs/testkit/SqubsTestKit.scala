81f406fa7e67dbf837c6c5e241a9f143fe73e355
asucharitakul <asucharitakul@ebay.com>
2013-12-09 15:26:21 -0800

    Added debug timeout management and proper logging in case of tests.

--- testkit/src/main/scala/org/squbs/testkit/SqubsTestKit.scala
+++ testkit/src/main/scala/org/squbs/testkit/SqubsTestKit.scala
@@ -1,5 +1,6 @@
 package org.squbs.testkit
 
+import scala.concurrent.duration._
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{Suite, BeforeAndAfterAll}
 import org.squbs.unicomplex.{Bootstrap, Unicomplex}
@@ -25,7 +26,24 @@ object SqubsTestKit {
 abstract class SqubsTestKit extends TestKit(Unicomplex.actorSystem)
 with ImplicitSender with Suite with BeforeAndAfterAll {
 
+  val debugTimeout = 10000.seconds
+
+  val debugMode = java.lang.management.ManagementFactory.getRuntimeMXBean.
+    getInputArguments.toString.indexOf("jdwp") >= 0
+
+  if (debugMode) println(
+    "\n##################\n" +
+    s"IMPORTANT: Detected system running in debug mode. All timeouts overridden to $debugTimeout.\n" +
+    "##################\n\n")
+
+
+
   override protected def beforeAll() {
     SqubsTestKit.checkInit(this)
   }
+
+  override def receiveOne(max: Duration) =
+    if (debugMode) super.receiveOne(debugTimeout)
+    else super.receiveOne(max)
+
 }
