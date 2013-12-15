cb080c8187a9279a3a906af44cfbbd9198e5de8c
asucharitakul <asucharitakul@ebay.com>
2013-12-14 20:25:48 -0800

    Added project sbt-squbs for plugin development.

---
+++ sbt-squbs/build.sbt
@@ -0,0 +1,9 @@
+sbtPlugin := true
+
+scalaVersion := "2.10.1"
+
+name := "sbt-squbs"
+
+organization := "org.squbs"
+
+version := "0.0.1"
---
+++ sbt-squbs/src/main/scala/org/squbs/plugin/SqubsPlugin.scala
@@ -0,0 +1,15 @@
+package org.squbs.plugin
+/*
+ * Copyright (c) 2013 eBay, Inc.
+ * All rights reserved.
+ *
+ * Contributors:
+ * asucharitakul
+ */
+import sbt._
+
+
+
+class SqubsPlugin extends Plugin {
+
+}
