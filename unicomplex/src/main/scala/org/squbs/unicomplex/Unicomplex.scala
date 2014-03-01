81b471f29378353608de4aaa547448991144b93c
asucharitakul <asucharitakul@ebay.com>
2014-03-01 00:14:19 -0800

    Cleaned up the shutdown to properly shutdown the web service.

--- unicomplex/src/main/scala/org/squbs/unicomplex/Unicomplex.scala
+++ unicomplex/src/main/scala/org/squbs/unicomplex/Unicomplex.scala
@@ -11,6 +11,7 @@ import akka.actor.OneForOneStrategy
 import akka.actor.Terminated
 import akka.pattern.pipe
 import spray.can.Http.Bound
+import spray.can.Http
 
 object Unicomplex {
 
@@ -102,10 +103,15 @@ class Unicomplex extends Actor with Stash with ActorLogging {
   private var serviceBound = false
 
   private def shutdownState: Receive = {
+
+    case Http.ClosedAll | Http.Unbound =>
+      serviceRef foreach (_ ! PoisonPill)
+      serviceRef = None
+
     case Terminated(target) => log.debug(s"$target is terminated")
       if (cubes contains target) {
         cubes -= target
-      }else if (serviceRef.find(_ == target) != None) {
+      } else if (serviceRef.exists(_ == target)) {
         serviceRef = None
       }
       if (cubes.isEmpty && serviceRef == None) {
@@ -158,8 +164,8 @@ class Unicomplex extends Actor with Stash with ActorLogging {
       updateSystemState(Stopping)
       serviceRef match {
         case Some(ref) =>
-          ServiceRegistry.registrar() ! PoisonPill
-          ref ! PoisonPill
+          ServiceRegistry.stopWebService(ref)
+//          ref ! PoisonPill
         case _ =>
       }
       cubes.foreach(_._1 ! GracefulStop)
