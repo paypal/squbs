e8a9eb5450e6d43f71abe1134d9b1eaf564fa03e
asucharitakul <asucharitakul@ebay.com>
2014-04-30 13:00:03 -0700

    Changed JMX name Cube to CubeState

--- unicomplex/src/main/scala/org/squbs/unicomplex/JMX.scala
+++ unicomplex/src/main/scala/org/squbs/unicomplex/JMX.scala
@@ -10,7 +10,7 @@ object JMX {
 
   val systemStateName = "org.squbs.unicomplex:type=SystemState"
   val cubesName       = "org.squbs.unicomplex:type=Cubes"
-  val cubeStateName   = "org.squbs.unicomplex:type=Cube,name="
+  val cubeStateName   = "org.squbs.unicomplex:type=CubeState,name="
   val contextsName    = "org.squbs.unicomplex:type=Contexts"
 
   implicit def string2objectName(name:String):ObjectName = new ObjectName(name)
