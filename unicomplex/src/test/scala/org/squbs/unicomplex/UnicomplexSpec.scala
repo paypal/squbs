4c65a813f3de25b9ee43c03cba6e14323f2f1f58
zhuwang <zhuwang@ebay.com>
2014-06-10 16:56:29 -0700

    Separate out the system state test

--- unicomplex/src/test/scala/org/squbs/unicomplex/UnicomplexSpec.scala
+++ unicomplex/src/test/scala/org/squbs/unicomplex/UnicomplexSpec.scala
@@ -21,14 +21,14 @@ import scala.util.Success
  */
 object UnicomplexSpec {
 
-  val dummyJarsDir = new File("unicomplex/src/test/resources/classpaths")
+  val dummyJarsDir = "unicomplex/src/test/resources/classpaths"
 
-  val classPaths =
-    if (dummyJarsDir.exists && dummyJarsDir.isDirectory) {
-      dummyJarsDir.listFiles().map(_.getAbsolutePath)
-    } else {
-      throw new RuntimeException("[UnicomplexSpec] There is no cube to be loaded")
-    }
+  val classPaths = Array(
+    "DummyCube",
+    "DummyCubeSvc",
+    "DummySvc",
+    "DummyExtensions.jar"
+  ) map (dummyJarsDir + "/" + _)
 
   import collection.JavaConversions._
 
@@ -138,49 +138,7 @@ class UnicomplexSpec extends TestKit(UnicomplexSpec.boot.actorSystem) with Impli
     }
   }
 
-  "CubeSupervisor" must {
-
-    "get init reports from cube actors" in {
-      system.actorSelection("/user/CubeA") ! CheckInitStatus
-      val report = expectMsgType[(InitReports, Boolean)]._1
-      report.state should be(Active)
-      report.reports.size should be(2)
-    }
-
-    "get init reports from cube actors even if the actor failed in init" in {
-      system.actorSelection("/user/InitFail") ! CheckInitStatus
-      val report = expectMsgType[(InitReports, Boolean)]._1
-      report.state should be(Failed)
-      report.reports.size should be(1)
-    }
-
-    "deal with the situation that cube actors are not able to send the reports" in {
-      system.actorSelection("/user/InitBlock") ! CheckInitStatus
-      val report = expectMsgType[(InitReports, Boolean)]._1
-      report.state should be(Initializing)
-      report.reports.size should be(1)
-    }
-  }
-
   "UniComplex" must {
-
-    "get cube init reports" in {
-      Unicomplex(system).uniActor ! ReportStatus
-      val (systemState, cubes) = expectMsgType[(LifecycleState, Map[ActorRef, (CubeRegistration, Option[InitReports])])]
-      systemState should be (Failed)
-      val cubeAReport = cubes.values.find(_._1.name == "CubeA").flatMap(_._2)
-      cubeAReport should not be (None)
-      assert(cubeAReport.get.state == Active)
-      val cubeBReport = cubes.values.find(_._1.name == "CubeB").flatMap(_._2)
-      cubeBReport should not be (None)
-      cubeBReport.get.state should be (Active)
-      val initFailReport = cubes.values.find(_._1.name == "InitFail").flatMap(_._2)
-      initFailReport should not be (None)
-      initFailReport.get.state should be (Failed)
-      val initBlockReport = cubes.values.find(_._1.name == "InitBlock").flatMap(_._2)
-      initBlockReport should not be (None)
-      initBlockReport.get.state should be (Initializing)
-    }
 
     "stop a single cube without affect other cubes" in {
       Unicomplex(system).uniActor ! StopCube("org.squbs.unicomplex.test.DummyCubeSvc")
