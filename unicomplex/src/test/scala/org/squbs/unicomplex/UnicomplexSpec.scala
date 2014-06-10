9e9940df76ed766d46a230f2797cec31a2b87928
zhuwang <zhuwang@ebay.com>
2014-06-10 14:49:12 -0700

    Try to fix test case failure

--- unicomplex/src/test/scala/org/squbs/unicomplex/UnicomplexSpec.scala
+++ unicomplex/src/test/scala/org/squbs/unicomplex/UnicomplexSpec.scala
@@ -1,7 +1,7 @@
 package org.squbs.unicomplex
 
 import akka.testkit.{ImplicitSender, TestKit}
-import org.scalatest.{WordSpecLike, Matchers, BeforeAndAfterAll}
+import org.scalatest._
 import java.io.{FileNotFoundException, File}
 import scala.concurrent.duration._
 import org.scalatest.concurrent.AsyncAssertions
@@ -13,6 +13,8 @@ import scala.util.{Success, Failure, Try}
 import org.squbs.unicomplex.dummyextensions.DummyExtension
 import org.squbs.lifecycle.GracefulStop
 import com.typesafe.config.ConfigFactory
+import scala.util.Failure
+import scala.util.Success
 
 /**
  * Created by zhuwang on 2/21/14.
@@ -32,7 +34,7 @@ object UnicomplexSpec {
 
   val mapConfig = ConfigFactory.parseMap(
     Map(
-      "squbs.actorsystem-name"    -> "initTest",
+      "squbs.actorsystem-name"    -> "UnicomplexSpec",
       "squbs." + JMX.prefixConfig -> Boolean.box(true)
     )
   )
@@ -46,7 +48,7 @@ object UnicomplexSpec {
 
 class UnicomplexSpec extends TestKit(UnicomplexSpec.boot.actorSystem) with ImplicitSender
                              with WordSpecLike with Matchers with BeforeAndAfterAll
-                             with AsyncAssertions {
+                             with BeforeAndAfterEach with AsyncAssertions with SequentialNestedSuiteExecution {
 
   import UnicomplexSpec._
 
@@ -56,8 +58,8 @@ class UnicomplexSpec extends TestKit(UnicomplexSpec.boot.actorSystem) with Impli
 
   implicit val executionContext = system.dispatcher
 
-  sys.addShutdownHook {
-    system.shutdown()
+  override def afterEach() {
+    println("----------------------------------------------------------------------------------------")
   }
 
   override def beforeAll() {
@@ -137,206 +139,98 @@ class UnicomplexSpec extends TestKit(UnicomplexSpec.boot.actorSystem) with Impli
   "CubeSupervisor" must {
 
     "get init reports from cube actors" in {
-      val w = new Waiter
-
-      var supervisor: ActorRef  = null
-      system.actorSelection("/user/CubeA").resolveOne().onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          supervisor = result.get
-        }
-        w.dismiss()
-      })
-      w.await()
-
-      val statusFuture = supervisor ? CheckInitStatus
-
-      statusFuture.onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get.isInstanceOf[(InitReports, Boolean)])
-          val report = result.get.asInstanceOf[(InitReports, Boolean)]._1
-          assert(report.state == Active)
-          assert(report.reports.size == 2)
-        }
-        w.dismiss()
-      })
-      w.await()
+      system.actorSelection("/user/CubeA") ! CheckInitStatus
+      val report = expectMsgType[(InitReports, Boolean)]._1
+      report.state should be(Active)
+      report.reports.size should be(2)
     }
 
     "get init reports from cube actors even if the actor failed in init" in {
-      val w = new Waiter
-
-      var supervisor: ActorRef  = null
-      system.actorSelection("/user/InitFail").resolveOne().onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          supervisor = result.get
-        }
-        w.dismiss()
-      })
-      w.await()
-
-      val statusFuture = supervisor ? CheckInitStatus
-
-      statusFuture.onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get.isInstanceOf[(InitReports, Boolean)])
-          val report = result.get.asInstanceOf[(InitReports, Boolean)]._1
-          assert(report.state == Failed)
-          assert(report.reports.size == 1)
-        }
-        w.dismiss()
-      })
-      w.await()
+      system.actorSelection("/user/InitFail") ! CheckInitStatus
+      val report = expectMsgType[(InitReports, Boolean)]._1
+      report.state should be(Failed)
+      report.reports.size should be(1)
     }
-  }
 
-  "deal with the situation that cube actors are not able to send the reports" in {
-    val w = new Waiter
-
-    var supervisor: ActorRef  = null
-    system.actorSelection("/user/InitBlock").resolveOne().onComplete(result => {
-      w {
-        assert(result.isSuccess)
-        supervisor = result.get
-      }
-      w.dismiss()
-    })
-    w.await()
-
-    val statusFuture = supervisor ? CheckInitStatus
-
-    statusFuture.onComplete(result => {
-      w {
-        assert(result.isSuccess)
-        assert(result.get.isInstanceOf[(InitReports, Boolean)])
-        val report = result.get.asInstanceOf[(InitReports, Boolean)]._1
-        assert(report.state == Initializing)
-        assert(report.reports.size == 1)
-      }
-      w.dismiss()
-    })
-    w.await()
+    "deal with the situation that cube actors are not able to send the reports" in {
+      system.actorSelection("/user/InitBlock") ! CheckInitStatus
+      val report = expectMsgType[(InitReports, Boolean)]._1
+      report.state should be(Initializing)
+      report.reports.size should be(1)
+    }
   }
 
   "UniComplex" must {
 
     "get cube init reports" in {
-      val w = new Waiter
-
-      val statusFuture = Unicomplex(system).uniActor ? ReportStatus
-
-      statusFuture.onComplete(report => {
-        w {
-          assert(report.isSuccess)
-          assert(report.get.isInstanceOf[(LifecycleState, Map[ActorRef, (CubeRegistration, Option[InitReports])])])
-          val (systemState, cubes) = report.get.asInstanceOf[(LifecycleState, Map[ActorRef, (CubeRegistration, Option[InitReports])])]
-          assert(systemState == Failed)
-          val cubeAReport = cubes.values.find(_._1.name == "CubeA").flatMap(_._2)
-          assert(cubeAReport != None)
-          assert(cubeAReport.get.state == Active)
-          val cubeBReport = cubes.values.find(_._1.name == "CubeB").flatMap(_._2)
-          assert(cubeBReport != None)
-          assert(cubeBReport.get.state == Active)
-          val initFailReport = cubes.values.find(_._1.name == "InitFail").flatMap(_._2)
-          assert(initFailReport != None)
-          assert(initFailReport.get.state == Failed)
-          val initBlockReport = cubes.values.find(_._1.name == "InitBlock").flatMap(_._2)
-          assert(initBlockReport != None)
-          assert(initBlockReport.get.state == Initializing)
-        }
-        w.dismiss()
-      })
-      w.await()
+      Unicomplex(system).uniActor ! ReportStatus
+      val (systemState, cubes) = expectMsgType[(LifecycleState, Map[ActorRef, (CubeRegistration, Option[InitReports])])]
+      systemState should be (Failed)
+      val cubeAReport = cubes.values.find(_._1.name == "CubeA").flatMap(_._2)
+      cubeAReport should not be (None)
+      assert(cubeAReport.get.state == Active)
+      val cubeBReport = cubes.values.find(_._1.name == "CubeB").flatMap(_._2)
+      cubeBReport should not be (None)
+      cubeBReport.get.state should be (Active)
+      val initFailReport = cubes.values.find(_._1.name == "InitFail").flatMap(_._2)
+      initFailReport should not be (None)
+      initFailReport.get.state should be (Failed)
+      val initBlockReport = cubes.values.find(_._1.name == "InitBlock").flatMap(_._2)
+      initBlockReport should not be (None)
+      initBlockReport.get.state should be (Initializing)
     }
 
     "stop a single cube without affect other cubes" in {
-
       assert(Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/ping").mkString equals "Pong")
       assert(Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/pong").mkString equals "Ping")
+      Unicomplex(system).uniActor ! StopCube("org.squbs.unicomplex.test.DummyCubeSvc")
+      expectMsg(Ack)
 
       val w = new Waiter
-
-      val stopCubeFuture = Unicomplex(system).uniActor ? StopCube("org.squbs.unicomplex.test.DummyCubeSvc")
-
-      stopCubeFuture.onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get == Ack)
-        }
-        w.dismiss()
-        println("Cube stop acknowledged.")
-      })
-
-      w.await()
-
       system.actorSelection("/user/DummyCubeSvc").resolveOne().onComplete(result => {
         w {assert(result.isFailure)}
         w.dismiss()
       })
+      w.await()
 
       assert(Source.fromURL(s"http://127.0.0.1:$port/dummysvc/msg/hello").mkString equals "^hello$")
-
       intercept[FileNotFoundException]{
         Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/ping").mkString
       }
-
       intercept[FileNotFoundException]{
         Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/pong").mkString
       }
-
-      w.await()
-
     }
 
     "not mess up if stop a stopped cube" in {
       assert(Source.fromURL(s"http://127.0.0.1:$port/dummysvc/msg/hello").mkString equals "^hello$")
-
       intercept[FileNotFoundException]{
         Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/ping").mkString
       }
-
       intercept[FileNotFoundException]{
         Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/pong").mkString
       }
 
+      Unicomplex(system).uniActor ! StopCube("org.squbs.unicomplex.test.DummyCubeSvc")
+      expectMsg(Ack)
+
       val w = new Waiter
-
-      val stopCubeFuture = Unicomplex(system).uniActor ? StopCube("org.squbs.unicomplex.test.DummyCubeSvc")
-
-      stopCubeFuture.onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get == Ack)
-        }
-        w.dismiss()
-      })
-
-      w.await()
-
       system.actorSelection("/user/DummyCubeSvc").resolveOne().onComplete(result => {
         w {assert(result.isFailure)}
         w.dismiss()
       })
+      w.await()
 
       assert(Source.fromURL(s"http://127.0.0.1:$port/dummysvc/msg/hello").mkString equals "^hello$")
-
       intercept[FileNotFoundException]{
         Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/ping").mkString
       }
-
       intercept[FileNotFoundException]{
         Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/pong").mkString
       }
-
-      w.await()
     }
 
     "start a single cube correctly" in {
-      val w = new Waiter
-
       intercept[FileNotFoundException]{
         Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/ping").mkString
       }
@@ -345,57 +239,35 @@ class UnicomplexSpec extends TestKit(UnicomplexSpec.boot.actorSystem) with Impli
         Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/pong").mkString
       }
 
-      val startCubeFuture =
-        Unicomplex(system).uniActor ? StartCube("org.squbs.unicomplex.test.DummyCubeSvc",
-          boot.initInfoMap, boot.listenerAliases)
-
-      startCubeFuture.onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get == Ack)
-        }
-        w.dismiss()
-      })
-
-      w.await()
+      Unicomplex(system).uniActor ! StartCube("org.squbs.unicomplex.test.DummyCubeSvc",
+        boot.initInfoMap, boot.listenerAliases)
+      expectMsg(Ack)
 
       Thread.sleep(1000)
 
+      val w = new Waiter
       system.actorSelection("/user/DummyCubeSvc").resolveOne().onComplete(result => {
         w {assert(result.isSuccess)}
         w.dismiss()
       })
-
       w.await()
 
       assert(Source.fromURL(s"http://127.0.0.1:$port/dummysvc/msg/hello").mkString equals "^hello$")
-
       assert(Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/ping").mkString equals "Pong")
       assert(Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/pong").mkString equals "Ping")
     }
 
     "not mess up if start a running cube" in {
+      Unicomplex(system).uniActor ! StartCube("org.squbs.unicomplex.test.DummyCubeSvc", boot.initInfoMap,
+        boot.listenerAliases)
+
+      expectMsg(Ack)
+
       val w = new Waiter
-
-      val startCubeFuture =
-        Unicomplex(system).uniActor ? StartCube("org.squbs.unicomplex.test.DummyCubeSvc", boot.initInfoMap,
-          boot.listenerAliases)
-
-      startCubeFuture.onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get == Ack)
-        }
-        w.dismiss()
-      })
-
-      w.await()
-
       system.actorSelection("/user/DummyCubeSvc").resolveOne().onComplete(result => {
         w {assert(result.isSuccess)}
         w.dismiss()
       })
-
       w.await()
 
       assert(Source.fromURL(s"http://127.0.0.1:$port/dummysvc/msg/hello").mkString equals "^hello$")
@@ -404,25 +276,14 @@ class UnicomplexSpec extends TestKit(UnicomplexSpec.boot.actorSystem) with Impli
     }
 
     "not mess up if stop and start a cube contains actors and services simultaneously" in {
-      val stopCubeFuture = Unicomplex(system).uniActor ? StopCube("org.squbs.unicomplex.test.DummyCubeSvc")
-      val startCubeFuture =
-        Unicomplex(system).uniActor ? StartCube("org.squbs.unicomplex.test.DummyCubeSvc", boot.initInfoMap,
-          boot.listenerAliases)
-
-      val w = new Waiter
-      Future.sequence(Seq(stopCubeFuture, startCubeFuture)).onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get.forall(_ == Ack))
-        }
-        w.dismiss()
-      })
-
-      w.await()
+      Unicomplex(system).uniActor ! StopCube("org.squbs.unicomplex.test.DummyCubeSvc")
+      Unicomplex(system).uniActor ! StartCube("org.squbs.unicomplex.test.DummyCubeSvc", boot.initInfoMap,
+        boot.listenerAliases)
+      expectMsg(Ack)
+      expectMsg(Ack)
 
       def svcReady = Try {
         assert(Source.fromURL(s"http://127.0.0.1:$port/dummysvc/msg/hello").mkString equals "^hello$")
-
         assert(Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/ping").mkString equals "Pong")
         assert(Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/pong").mkString equals "Ping")
       } match {
@@ -437,25 +298,14 @@ class UnicomplexSpec extends TestKit(UnicomplexSpec.boot.actorSystem) with Impli
       }
 
       if (retry == 0) throw new Exception("service timeout in 5s")
-
     }
 
     "not mess up if stop and start a cube contains actors only simultaneously" in {
-      val stopCubeFuture = Unicomplex(system).uniActor ? StopCube("org.squbs.unicomplex.test.DummyCube")
-      val startCubeFuture = Unicomplex(system).uniActor ?
-        StartCube("org.squbs.unicomplex.test.DummyCube", boot.initInfoMap,
+      Unicomplex(system).uniActor ! StopCube("org.squbs.unicomplex.test.DummyCube")
+      Unicomplex(system).uniActor ! StartCube("org.squbs.unicomplex.test.DummyCube", boot.initInfoMap,
           boot.listenerAliases)
-
-      val w = new Waiter
-      Future.sequence(Seq(stopCubeFuture, startCubeFuture)).onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get.forall(_ == Ack))
-        }
-        w.dismiss()
-      })
-
-      w.await()
+      expectMsg(Ack)
+      expectMsg(Ack)
 
       def svcReady = Try {
         assert(Source.fromURL(s"http://127.0.0.1:$port/dummysvc/msg/hello").mkString equals "^hello$")
@@ -476,21 +326,11 @@ class UnicomplexSpec extends TestKit(UnicomplexSpec.boot.actorSystem) with Impli
     }
 
     "not mess up if stop and start a cube contains services only simultaneously" in {
-      val stopCubeFuture = Unicomplex(system).uniActor ? StopCube("org.squbs.unicomplex.test.DummySvc")
-      val startCubeFuture =
-        Unicomplex(system).uniActor ? StartCube("org.squbs.unicomplex.test.DummySvc", boot.initInfoMap,
-          boot.listenerAliases)
-
-      val w = new Waiter
-      Future.sequence(Seq(stopCubeFuture, startCubeFuture)).onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get.forall(_ == Ack))
-        }
-        w.dismiss()
-      })
-
-      w.await()
+      Unicomplex(system).uniActor ! StopCube("org.squbs.unicomplex.test.DummySvc")
+      Unicomplex(system).uniActor ! StartCube("org.squbs.unicomplex.test.DummySvc", boot.initInfoMap,
+        boot.listenerAliases)
+      expectMsg(Ack)
+      expectMsg(Ack)
 
       assert(Source.fromURL(s"http://127.0.0.1:$port/dummysvc/msg/hello").mkString equals "^hello$")
       assert(Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/ping").mkString equals "Pong")
@@ -498,50 +338,34 @@ class UnicomplexSpec extends TestKit(UnicomplexSpec.boot.actorSystem) with Impli
     }
 
     "not mess up if stop all cubes simultaneously" in {
-      val w = new Waiter
-
-      Future.sequence(Seq(
-        Unicomplex(system).uniActor ? StopCube("org.squbs.unicomplex.test.DummySvc"),
-        Unicomplex(system).uniActor ? StopCube("org.squbs.unicomplex.test.DummyCube"),
-        Unicomplex(system).uniActor ? StopCube("org.squbs.unicomplex.test.DummyCubeSvc")
-      )).onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get.forall(_ == Ack))
-        }
-        w.dismiss()
-      })
+      Unicomplex(system).uniActor ! StopCube("org.squbs.unicomplex.test.DummySvc")
+      Unicomplex(system).uniActor ! StopCube("org.squbs.unicomplex.test.DummyCube")
+      Unicomplex(system).uniActor ! StopCube("org.squbs.unicomplex.test.DummyCubeSvc")
+      expectMsg(Ack)
+      expectMsg(Ack)
+      expectMsg(Ack)
 
       intercept[FileNotFoundException]{
         Source.fromURL(s"http://127.0.0.1:$port/dummysvc/msg/hello").mkString
       }
-
       intercept[FileNotFoundException]{
         Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/ping").mkString
       }
-
       intercept[FileNotFoundException]{
         Source.fromURL(s"http://127.0.0.1:$port/pingpongsvc/pong").mkString
       }
     }
 
     "not mess up if start all cubes simultaneously" in {
-      val w = new Waiter
-
-      Future.sequence(Seq(
-        Unicomplex(system).uniActor ? StartCube("org.squbs.unicomplex.test.DummySvc", boot.initInfoMap,
-          boot.listenerAliases),
-        Unicomplex(system).uniActor ? StartCube("org.squbs.unicomplex.test.DummyCube", boot.initInfoMap,
-          boot.listenerAliases),
-        Unicomplex(system).uniActor ? StartCube("org.squbs.unicomplex.test.DummyCubeSvc", boot.initInfoMap,
-          boot.listenerAliases)
-      )).onComplete(result => {
-        w {
-          assert(result.isSuccess)
-          assert(result.get.forall(_ == Ack))
-        }
-        w.dismiss()
-      })
+      Unicomplex(system).uniActor ! StartCube("org.squbs.unicomplex.test.DummySvc", boot.initInfoMap,
+        boot.listenerAliases)
+      Unicomplex(system).uniActor ! StartCube("org.squbs.unicomplex.test.DummyCube", boot.initInfoMap,
+        boot.listenerAliases)
+      Unicomplex(system).uniActor ! StartCube("org.squbs.unicomplex.test.DummyCubeSvc", boot.initInfoMap,
+        boot.listenerAliases)
+      expectMsg(Ack)
+      expectMsg(Ack)
+      expectMsg(Ack)
 
       def svcReady = Try {
         assert(Source.fromURL(s"http://127.0.0.1:$port/dummysvc/msg/hello").mkString equals "^hello$")
