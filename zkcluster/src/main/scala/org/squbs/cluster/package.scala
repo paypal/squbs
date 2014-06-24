d292c0f1ca0fba5117c6d2b09f4b4cffbceaee71
huzhou <huzhou@ebay.com>
2014-06-23 17:31:19 -0700

    ~ bugfix continued

--- zkcluster/src/main/scala/org/squbs/cluster/package.scala
+++ zkcluster/src/main/scala/org/squbs/cluster/package.scala
@@ -163,12 +163,12 @@ package object cluster {
                                     partitionsToMembers:Map[ByteString, Set[Address]],
                                     changed:ZkPartitionsChanged) = {
     val impacted = partitionsToMembers.filterKeys(segmentsToPartitions.getOrElse(changed.segment, Set.empty).contains(_)).keySet
-    val onboards = changed.partitions.keySet.filter{partitionKey => partitionsToMembers.getOrElse(partitionKey, Set.empty).size <= changed.partitions.getOrElse(partitionKey, Set.empty).size}
+    val onboards = changed.partitions.keySet.filter{partitionKey => partitionsToMembers.getOrElse(partitionKey, Set.empty).size != changed.partitions.getOrElse(partitionKey, Set.empty).size}
     val dropoffs = impacted.diff(changed.partitions.keySet) ++ changed.partitions.keySet.filter{partitionKey => changed.partitions.getOrElse(partitionKey, Set.empty).isEmpty}
 
     //drop off members no longer in the partition
     ((partitionsToMembers ++ changed.partitions).filterKeys(!dropoffs.contains(_)),
-      onboards,
+      onboards -- dropoffs,
       dropoffs)
   }
 
