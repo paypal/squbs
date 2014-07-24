08c18fd9a646235dcc7c151409d9e58f5757ff63
asucharitakul <asucharitakul@ebay.com>
2014-07-23 18:54:49 -0700

    https://jirap.corp.ebay.com/browse/SQUBS-160: Added streaming support to DummySvcActor.

--- unicomplex/src/test/scala/org/squbs/unicomplex/dummysvcactor/DummySvcActor.scala
+++ unicomplex/src/test/scala/org/squbs/unicomplex/dummysvcactor/DummySvcActor.scala
@@ -1,8 +1,9 @@
 package org.squbs.unicomplex.dummysvcactor
 
-import akka.actor.{ActorLogging, Actor}
+import akka.actor.{Props, ActorLogging, Actor}
+import spray.can.Http.RegisterChunkHandler
 import spray.http.StatusCodes._
-import spray.http.{HttpResponse, HttpRequest}
+import spray.http._
 
 class DummySvcActor extends Actor with ActorLogging {
 
@@ -10,5 +11,32 @@ class DummySvcActor extends Actor with ActorLogging {
     case req: HttpRequest if req.uri.path.toString == "/dummysvcactor/ping" =>
       log.debug("Received request " + req.uri)
       sender() ! HttpResponse(OK, "pong")
+
+    case ChunkedRequestStart(req) if req.uri.path.toString == "/dummysvcactor/chunks" =>
+      val handler = context.actorOf(Props[ChunkedRequestHandler])
+      sender() ! RegisterChunkHandler(handler)
+      handler forward req
+  }
+}
+
+class ChunkedRequestHandler extends Actor with ActorLogging {
+
+  var chunkCount = 0
+  var byteCount = 0
+
+  def receivedChunk(data: HttpData) {
+    if (data.length > 0) {
+      chunkCount += 1
+      byteCount += data.length
+    }
+  }
+
+  def receive = {
+    case req: HttpRequest => receivedChunk(req.entity.data)
+
+    case chunk: MessageChunk => receivedChunk(chunk.data)
+
+    case chunkEnd: ChunkedMessageEnd =>
+      sender() ! HttpResponse(OK, s"Received $chunkCount chunks and $byteCount bytes.")
   }
 }
