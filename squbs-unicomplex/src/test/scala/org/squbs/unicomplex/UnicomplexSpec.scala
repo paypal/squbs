cc7b4360f8951cb19c14e3605198a38ca4b038f4
jiamzhang <jiamzhang@ebay.com>
2014-08-08 10:33:28 +0800

    Fix testcase problem.

--- squbs-unicomplex/src/test/scala/org/squbs/unicomplex/UnicomplexSpec.scala
+++ squbs-unicomplex/src/test/scala/org/squbs/unicomplex/UnicomplexSpec.scala
@@ -23,7 +23,7 @@ import scala.util.Try
  */
 object UnicomplexSpec {
 
-  val dummyJarsDir = "unicomplex/src/test/resources/classpaths"
+  val dummyJarsDir = "squbs-unicomplex/src/test/resources/classpaths"
 
   val classPaths = Array(
     "DummyCube",
