028a90523d36bf61674eb657ea6d8251b6deee45
lma <lma@ebay.com>
2015-04-17 18:49:35 +0800

    add PipelineHandlerManager

--- squbs-unicomplex/src/main/scala/org/squbs/proxy/SimpleProcessor.scala
+++ squbs-unicomplex/src/main/scala/org/squbs/proxy/SimpleProcessor.scala
@@ -17,74 +17,66 @@
  */
 package org.squbs.proxy
 
-import akka.actor.{ActorContext, ActorRefFactory}
-import com.typesafe.config.{Config, ConfigFactory, ConfigValue}
+import akka.actor.{ActorContext, ActorRefFactory, ActorSystem}
+import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import org.squbs.pipeline._
+import org.squbs.unicomplex.ConfigUtil._
 
-import scala.collection.JavaConversions._
-import scala.collection.mutable.{HashMap => HMap}
 import scala.concurrent.{ExecutionContext, Future}
 
 case class SimplePipelineConfig(reqPipe: Seq[_ <: Handler], respPipe: Seq[_ <: Handler])
 
 case class SimpleProcessor(pipeConf: SimplePipelineConfig) extends Processor {
-	//inbound processing
-	def inbound(reqCtx: RequestContext)(implicit executor: ExecutionContext, context: ActorContext): Future[RequestContext] = {
-		pipeConf.reqPipe.foldLeft(Future.successful(reqCtx)) { (ctx, handler) => ctx flatMap (handler.process(_))}
-	}
+  //inbound processing
+  def inbound(reqCtx: RequestContext)(implicit executor: ExecutionContext, context: ActorContext): Future[RequestContext] = {
+    pipeConf.reqPipe.foldLeft(Future.successful(reqCtx)) { (ctx, handler) => ctx flatMap (handler.process(_))}
+  }
 
-	//outbound processing
-	def outbound(reqCtx: RequestContext)(implicit executor: ExecutionContext, context: ActorContext): Future[RequestContext] = {
-		pipeConf.respPipe.foldLeft(Future.successful(reqCtx)) { (ctx, handler) => ctx.flatMap(handler.process(_))}
-	}
+  //outbound processing
+  def outbound(reqCtx: RequestContext)(implicit executor: ExecutionContext, context: ActorContext): Future[RequestContext] = {
+    pipeConf.respPipe.foldLeft(Future.successful(reqCtx)) { (ctx, handler) => ctx.flatMap(handler.process(_))}
+  }
 }
 
 object SimpleProcessor {
-	def empty: SimpleProcessor = new SimpleProcessor(SimplePipelineConfig.empty)
+  def empty: SimpleProcessor = new SimpleProcessor(SimplePipelineConfig.empty)
 }
 
 class SimpleProcessorFactory extends ProcessorFactory {
-	def create(settings: Option[Config])(implicit actorRefFactory: ActorRefFactory): Option[Processor] = {
-		settings match {
-			case Some(conf) => Some(SimpleProcessor(SimplePipelineConfig(conf)))
-			case _ => None
-		}
-	}
+  def create(settings: Option[Config])(implicit actorRefFactory: ActorRefFactory): Option[Processor] = {
+    settings match {
+      case Some(conf) => Some(SimpleProcessor(SimplePipelineConfig(conf)))
+      case _ => None
+    }
+  }
 }
 
 object SimplePipelineConfig extends LazyLogging {
 
-	def apply(config: Config): SimplePipelineConfig = {
-		import org.squbs.unicomplex.ConfigUtil._
+  def apply(config: Config)(implicit actorRefFactory: ActorRefFactory): SimplePipelineConfig = {
 
-		val handlerConf = config.getOptionalConfig("handlers").getOrElse(ConfigFactory.empty)
-		val handlerCache = new HMap[String, Handler]()
-		handlerConf.root.foreach {
-			case (name, hconf: ConfigValue) =>
-				val clazz = hconf.unwrapped.toString
-				try {
-					handlerCache += (name -> Class.forName(clazz).newInstance.asInstanceOf[Handler])
-				} catch {
-					case t: Throwable =>
-						logger.error("Can't instantiate the handler with name of:" + name)
-				}
-			case _ => // ignore
-		}
+    val system: Option[ActorSystem] = actorRefFactory match {
+      case system: ActorSystem => Some(system)
+      case ctx: ActorContext => Some(ctx.system)
+      case other =>
+        logger.error(s"Unsupported actorRefFactory: ${other.getClass.getName}")
+        None
+    }
+    system.fold(SimplePipelineConfig.empty) {
+      sys =>
+        val mgr = PipelineHandlerManager(sys)
+        val reqPipe = config.getOptionalStringList("inbound").getOrElse(Seq.empty)
+        val respPipe = config.getOptionalStringList("outbound").getOrElse(Seq.empty)
+        val reqPipeObj = reqPipe.flatMap { h =>
+          mgr.get(h)
+        }
+        val respPipeObj = respPipe.flatMap { h =>
+          mgr.get(h)
+        }
+        SimplePipelineConfig(reqPipeObj, respPipeObj)
+    }
+  }
 
-		val reqPipe = config.getOptionalStringList("inbound").getOrElse(Seq.empty)
-		val respPipe = config.getOptionalStringList("outbound").getOrElse(Seq.empty)
-
-		val reqPipeObj = reqPipe.flatMap { h =>
-			handlerCache.get(h)
-		}
-
-		val respPipeObj = respPipe.flatMap { h =>
-			handlerCache.get(h)
-		}
-
-		SimplePipelineConfig(reqPipeObj, respPipeObj)
-	}
-
-	def empty = SimplePipelineConfig(Seq.empty, Seq.empty)
+  def empty = SimplePipelineConfig(Seq.empty, Seq.empty)
 }
