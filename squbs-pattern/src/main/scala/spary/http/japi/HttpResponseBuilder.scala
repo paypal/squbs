d57182d1290f9d938abfd78876d08a53425b8715
Wang Miaomiao <miawang@ebay.com>
2015-06-16 14:47:28 +0800

    Java 8 wrapper for spray's HttpResponse, ContentType and HttpHeader

---
+++ squbs-pattern/src/main/scala/spary/http/japi/HttpResponseBuilder.scala
@@ -0,0 +1,101 @@
+/*
+ * Licensed to Typesafe under one or more contributor license agreements.
+ * See the AUTHORS file distributed with this work for
+ * additional information regarding copyright ownership.
+ * This file is licensed to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package spary.http.japi
+
+import java.util.Collections
+
+import com.typesafe.scalalogging.LazyLogging
+import spray.http.HttpHeaders.RawHeader
+import spray.http._
+import spray.http.parser.HttpParser
+import collection.JavaConversions._
+
+class HttpResponseBuilder {
+  private var status: StatusCode = StatusCodes.OK
+  private var entity: HttpEntity = HttpEntity.Empty
+  private var headers: java.util.List[HttpHeader] = Collections.emptyList()
+  private var protocol: HttpProtocol = HttpProtocols.`HTTP/1.1`
+
+  def status(status: StatusCode): HttpResponseBuilder = {
+    require(status != null, "status can't be null")
+    this.status = status
+    this
+  }
+
+  def entity(entity: HttpEntity): HttpResponseBuilder = {
+    require(entity != null, "entity can't be null")
+    this.entity = entity
+    this
+  }
+
+  def entity(contentType: ContentType, data: String): HttpResponseBuilder = this.entity(HttpEntity(contentType, data))
+
+  def entity(contentType: ContentType, data: Array[Byte]): HttpResponseBuilder = this
+    .entity(HttpEntity(contentType, data))
+
+  def entity(data: String): HttpResponseBuilder = this.entity(HttpEntity(data))
+
+  def entity(data: Array[Byte]): HttpResponseBuilder = this.entity(HttpEntity(data))
+
+  def headers(headers: java.util.List[HttpHeader]): HttpResponseBuilder = {
+    if (headers != null) this.headers = headers
+    this
+  }
+
+  def protocol(protocol: String): HttpResponseBuilder = {
+    require(protocol != null, "protocol can't be null")
+    this.protocol = HttpProtocols.getForKey(protocol).get
+    this
+  }
+
+  def build() = HttpResponse(status, entity, headers.toList, protocol)
+}
+
+object ContentTypeFactory {
+  /**
+   *
+   * @param contentType value like <code>application/json</code> or <code>application/json; charset=utf-8</code>
+   * @return
+   */
+  def create(contentType: String) = {
+    HttpParser.parse(HttpParser.ContentTypeHeaderValue, contentType) match {
+      case Right(c) => c
+      case Left(error) =>
+        throw new IllegalArgumentException(
+          error.withSummaryPrepended(s"Invalid content-type value:$contentType").summary)
+    }
+  }
+}
+
+
+object HttpHeaderFactory extends LazyLogging {
+  def create(name: String, value: String) = {
+    HttpParser.parserRules.get(name.toLowerCase) match {
+      case Some(rule) => HttpParser.parse(rule, value) match {
+        // try ModeledHeader first
+        case Right(h) => h
+        case Left(e) =>
+          // fallback to RawHeader if parsing failed
+          // following the logic in spray.can.parsing.HttpHeaderParser.modelledHeaderValueParser
+          logger.warn(e.withSummaryPrepended(s"Illegal '$name' header").summary)
+          RawHeader(name, value)
+      }
+      case _ => RawHeader(name, value)
+    }
+  }
+}
---
+++ squbs-pattern/src/test/scala/spary/http/japi/JApiTest.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to Typesafe under one or more contributor license agreements.
+ * See the AUTHORS file distributed with this work for
+ * additional information regarding copyright ownership.
+ * This file is licensed to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package spary.http.japi;
+
+import org.junit.Test;
+import spray.http.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class JApiTest {
+
+    @Test
+    public void testContentType() throws Exception {
+        try {
+            ContentTypeFactory.create("unknown");
+            fail("should throw exception");
+        } catch (IllegalArgumentException e) {
+            // PASS
+        }
+
+        ContentType contentType = ContentTypeFactory.create("application/json; charset=UTF-8");
+        assertEquals(ContentType$.MODULE$.apply(MediaTypes.application$divjson(), HttpCharsets.UTF$minus8()), contentType);
+
+        ContentType contentType1 = ContentTypeFactory.create("application/javascript");
+        assertEquals(ContentType$.MODULE$.apply(MediaTypes.application$divjavascript()), contentType1);
+
+    }
+
+    @Test
+    public void testHttpHeaders() throws Exception {
+        HttpHeader contentType = HttpHeaderFactory.create("Content-type", "application/json");
+        assertTrue(contentType instanceof HttpHeaders.Content$minusType);
+        assertTrue(contentType.is("content-type"));
+        assertEquals("application/json", contentType.value());
+
+        HttpHeader custom = HttpHeaderFactory.create("ABC", "def");
+        assertTrue(custom instanceof HttpHeaders.RawHeader);
+        assertTrue(custom.is("abc"));
+        assertEquals("def", custom.value());
+    }
+
+    @Test
+    public void testHttpResponse() throws Exception {
+        HttpResponse defaultRes = new HttpResponseBuilder().build();
+        assertEquals(StatusCodes.OK(), defaultRes.status());
+        assertTrue(defaultRes.entity().isEmpty());
+        assertTrue(defaultRes.headers().isEmpty());
+        assertEquals(HttpProtocols.HTTP$div1$u002E1(), defaultRes.protocol());
+
+        HttpResponse notFound = new HttpResponseBuilder().status(StatusCodes.NotFound()).build();
+        assertEquals(404, notFound.status().intValue());
+
+        HttpResponse normalResponse = new HttpResponseBuilder().entity("abc").build();
+        assertEquals("abc", normalResponse.entity().asString());
+        assertEquals(ContentTypeFactory.create("text/plain; charset=UTF-8"), ((HttpEntity.NonEmpty)normalResponse.entity()).contentType());
+
+        HttpResponse streamResponse = new HttpResponseBuilder().entity("abc".getBytes()).build();
+        assertEquals("abc", streamResponse.entity().asString());
+        assertEquals(ContentTypeFactory.create("application/octet-stream"), ((HttpEntity.NonEmpty)streamResponse.entity()).contentType());
+
+        ContentType contentType = ContentTypeFactory.create("application/json");
+        HttpResponse withContentType = new HttpResponseBuilder().entity(contentType, "{'a':1}").build();
+        assertEquals("{'a':1}", withContentType.entity().asString());
+        assertEquals(contentType, ((HttpEntity.NonEmpty)withContentType.entity()).contentType());
+
+        ArrayList<HttpHeader> httpHeaders = new ArrayList<>();
+        httpHeaders.add(HttpHeaderFactory.create("x-abc", "def"));
+        HttpResponse withHeaders = new HttpResponseBuilder().headers(httpHeaders).build();
+        assertEquals(1, withHeaders.headers().length());
+        assertEquals(httpHeaders.get(0), withHeaders.headers().apply(0));
+
+        HttpResponse withProtocol = new HttpResponseBuilder().protocol("HTTP/1.0").build();
+        assertEquals(HttpProtocols.HTTP$div1$u002E0(), withProtocol.protocol());
+    }
+}
