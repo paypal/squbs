a244a412392d69ee8b05e429bf159c82bc31eca0
Wang Miaomiao <miawang@ebay.com>
2015-06-19 09:51:57 +0800

    build the Route only once on first use

---
+++ squbs-unicomplex/src/test/scala/org/squbs/unicomplex/RouteDefinitionSpec.scala
@@ -0,0 +1,143 @@
+/*
+ * Licensed to Typesafe under one or more contributor license agreements.
+ * See the AUTHORS file distributed with this work for
+ * additional information regarding copyright ownership.
+ * This file is licensed to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.squbs.unicomplex
+
+import java.util.concurrent.TimeUnit
+
+import akka.actor.ActorSystem
+import akka.io.IO
+import akka.testkit.{ImplicitSender, TestKit}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfterAll, Matchers, FlatSpecLike}
+import org.squbs.lifecycle.GracefulStop
+import spray.can.Http
+import spray.http._
+import spray.routing._
+import Directives._
+import spray.util.Utils
+import concurrent.duration._
+
+import scala.util.Try
+
+object RouteDefinitionSpec {
+
+  val dummyJarsDir = getClass.getClassLoader.getResource("classpaths").getPath
+
+  val classPaths = Array(
+    "RouteDefinitionSpec"
+  ) map (dummyJarsDir + "/" + _)
+
+  val (_, port) = Utils.temporaryServerHostnameAndPort()
+
+  val config = ConfigFactory.parseString(
+    s"""
+       |squbs {
+       |  actorsystem-name = routeDef
+       |  ${JMX.prefixConfig} = true
+       |}
+       |default-listener.bind-port = $port
+    """.stripMargin
+  )
+
+  val boot = UnicomplexBoot(config)
+    .createUsing {(name, config) => ActorSystem(name, config)}
+    .scanComponents(classPaths)
+    .initExtensions.start()
+
+}
+
+class TestRouteDefinition extends RouteDefinition {
+  var count = 0
+  def route =
+    path ("first") {
+      get {
+        count += 1 // executed on build time
+        complete{count.toString}
+      }
+    } ~ path("second") {
+      get {
+        count += 1 // executed on build time
+        complete{count.toString}
+      }
+    } ~ path("third") {
+      get {
+        complete {
+          count += 1 // executed on invocation time
+          count.toString
+        }
+      }
+    }
+
+}
+
+
+class RouteDefinitionSpec extends TestKit(
+  RouteDefinitionSpec.boot.actorSystem) with FlatSpecLike with Matchers with ImplicitSender with BeforeAndAfterAll {
+
+  implicit val timeout: akka.util.Timeout =
+    Try(System.getProperty("test.timeout").toLong) map { millis =>
+      akka.util.Timeout(millis, TimeUnit.MILLISECONDS)
+    } getOrElse (10 seconds)
+
+  val port = system.settings.config getInt "default-listener.bind-port"
+
+  implicit val executionContext = system.dispatcher
+
+  override def afterAll() {
+    Unicomplex(system).uniActor ! GracefulStop
+  }
+
+  "The test actor" should "return correct count value" in {
+    // the count should be 2 due to two statements in first and second build time
+    IO(Http) ! HttpRequest(HttpMethods.GET, Uri(s"http://127.0.0.1:$port/routedef/first"))
+    within(timeout.duration) {
+      val response = expectMsgType[HttpResponse]
+      response.status should be(StatusCodes.OK)
+      response.entity.asString should be("2")
+    }
+
+    IO(Http) ! HttpRequest(HttpMethods.GET, Uri(s"http://127.0.0.1:$port/routedef/second"))
+    within(timeout.duration) {
+      val response = expectMsgType[HttpResponse]
+      response.status should be(StatusCodes.OK)
+      response.entity.asString should be("2")
+    }
+
+    // should be increase 1 on hitting third
+    IO(Http) ! HttpRequest(HttpMethods.GET, Uri(s"http://127.0.0.1:$port/routedef/third"))
+    within(timeout.duration) {
+      val response = expectMsgType[HttpResponse]
+      response.status should be(StatusCodes.OK)
+      response.entity.asString should be("3")
+    }
+
+    IO(Http) ! HttpRequest(HttpMethods.GET, Uri(s"http://127.0.0.1:$port/routedef/third"))
+    within(timeout.duration) {
+      val response = expectMsgType[HttpResponse]
+      response.status should be(StatusCodes.OK)
+      response.entity.asString should be("4")
+    }
+
+    IO(Http) ! HttpRequest(HttpMethods.GET, Uri(s"http://127.0.0.1:$port/routedef/second"))
+    within(timeout.duration) {
+      val response = expectMsgType[HttpResponse]
+      response.status should be(StatusCodes.OK)
+      response.entity.asString should be("4")
+    }
+  }
+}
