8ca6ca4e73660f1bae8fdab134dd0426d112bdb9
leon ma <lma@ebay.com>
2015-07-16 01:35:07 -0700

    add helper method for unmarshaller

--- squbs-pattern/src/main/scala/org/squbs/pattern/spray/japi/MarshallerHelper.scala
+++ squbs-pattern/src/main/scala/org/squbs/pattern/spray/japi/MarshallerHelper.scala
@@ -1,6 +1,8 @@
 package org.squbs.pattern.spray.japi
 
-import spray.httpx.marshalling.{ToResponseMarshaller, Marshaller}
+import spray.httpx.marshalling.{Marshaller, ToResponseMarshaller}
+import spray.httpx.unmarshalling.UnmarshallerLifting._
+import spray.httpx.unmarshalling._
 
 /**
  * Created by lma on 7/15/2015.
@@ -9,6 +11,10 @@ object MarshallerHelper {
 
   def toResponseMarshaller[T](mashaller: Marshaller[T]): ToResponseMarshaller[T] = {
     ToResponseMarshaller.liftMarshaller(mashaller)
+  }
+
+  implicit def toFromResponseUnmarshaller[R](unmarshaller: Unmarshaller[R]): FromResponseUnmarshaller[R] = {
+    fromResponseUnmarshaller(fromMessageUnmarshaller(unmarshaller))
   }
 
 }
