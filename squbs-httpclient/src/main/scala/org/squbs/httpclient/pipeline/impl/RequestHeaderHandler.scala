d2fe912b1866ce6aab04d72b92799fa77a5aa487
leon ma <lma@ebay.com>
2015-07-28 02:26:36 -0700

    refactor httpclient to use 1 signle header handler

--- squbs-httpclient/src/main/scala/org/squbs/httpclient/pipeline/impl/RequestHeaderHandler.scala
+++ squbs-httpclient/src/main/scala/org/squbs/httpclient/pipeline/impl/RequestHeaderHandler.scala
@@ -19,6 +19,7 @@ package org.squbs.httpclient.pipeline.impl
 import akka.actor.ActorContext
 import org.squbs.pipeline.{Handler, RequestContext}
 import spray.http.HttpHeader
+import scala.collection.parallel.mutable
 import scala.concurrent.{ExecutionContext, Future}
 
 class RequestAddHeaderHandler(httpHeader: HttpHeader) extends Handler {
@@ -51,3 +52,25 @@ class RequestUpdateHeaderHandler(httpHeader: HttpHeader) extends Handler {
 		}
 	}
 }
+
+class RequestUpdateHeadersHandler(newHeaders: List[HttpHeader]) extends Handler {
+  override def process(reqCtx: RequestContext)(implicit executor: ExecutionContext, context: ActorContext): Future[RequestContext] = {
+    val originalHeaders = reqCtx.request.headers
+    val newHeaderMap = scala.collection.mutable.Map.empty[String, HttpHeader]
+    newHeaders.foreach {
+      h => newHeaderMap.put(h.lowercaseName, h)
+    }
+
+    var finalHeaders: List[HttpHeader] = originalHeaders.map {
+      h => newHeaderMap.remove(h.lowercaseName) match {
+        case None => h
+        case Some(newHeader) => newHeader
+      }
+    }
+
+    if (newHeaderMap.size > 0) finalHeaders ++= newHeaderMap.values
+
+    Future.successful(reqCtx.copy(request = reqCtx.request.copy(headers = finalHeaders)))
+  }
+}
+
