dc542c11db2e94d59caf85136ba4394182999cce
Sakthipriyan Vairamani <thefourtheye@users.noreply.github.com>
2017-02-23 00:39:43 +0530

    https://github.com/paypal/squbs/issues/403: replace copyright year 2015 with 2017 (#404)
    Fixes: https://github.com/paypal/squbs/issues/403
    LGTM

--- squbs-unicomplex/src/test/scala/org/squbs/unicomplex/ShutdownSpec.scala
+++ squbs-unicomplex/src/test/scala/org/squbs/unicomplex/ShutdownSpec.scala
@@ -1,6 +1,6 @@
 
 /*
- * Copyright 2015 PayPal
+ * Copyright 2017 PayPal
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
