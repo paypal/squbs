dd4898a74633a578c539e5bcdcb5f174eb2c5342
Sherif Ebady <sebady@paypal.com>
2017-08-21 13:21:14 -0500

    #417: Add Retry Flow support
    Add a retry BIDI flow to support retrying if downstream failures
    Add support in retry for a failureDecider to allow clients to customize what constitues failure
    Update to use inputbuffer max for upper bound on registry size
    
    (cherry picked from commit b3bb64)

---
+++ docs/flow-retry.md
@@ -0,0 +1,169 @@
+# Retry stage
+
+### Overview
+
+Some stream use cases may require retrying of requests after a failure response.  squbs provides a `RetryBidi` Akka Streams stage to add a retry capability to
+ streams that need to add retries for any failing requests.
+
+### Dependency
+
+Add the following dependency to your `build.sbt` or scala build file:
+
+```
+"org.squbs" %% "squbs-ext" % squbsVersion
+```
+
+### Usage
+
+The retry stage functionality is provided via a `BidiFlow` that can be connected to flows via the `join` operator.  The retry `BidiFlow` will perform some
+ specified maximum number of retries of any failures from downstream. A failure is determined by either a passed in failure decider function or a Failure,
+ if a failure decider function is not supplied.
+
+If all the retried attempts fail then the last failure for that request is emitted.  The retry stage requires a `Context` to be carried around for each
+ element.  This is needed to uniquely identify each element (see [Context to Unique Id Mapping](#context-to-unique-id-mapping) section for more details).
+
+##### Scala
+
+```scala
+val retryBidi = RetryBidi[String, Long](maxRetries = 10)
+val flow = Flow[Try[String], Long].map(s => findAnEnglishWordThatStartWith(s))
+
+Source("a" :: "b" :: "c" :: Nil)
+  .via(retryBidi.join(flow))
+  .runWith(Sink.seq)
+```
+
+##### Java
+
+```java
+final BidiFlow<Pair[String, Context], Pair[String, Context],
+    Pair[Try[String], Context], Pair[Try[String], Context], NotUsed> retryBidi =
+    RetryBidi.create(2);
+
+final Flow<String, String, NotUsed> flow =
+    Flow.<String>create().map(s -> findAnEnglishWordThatStartWith(s));
+
+Source.from(Arrays.asList("a", "b", "c"))
+    .via(retryBidi.join(flow))
+    .runWith(Sink.seq(), mat);
+```
+
+#### Context to Unique Id Mapping
+
+The `Context` type itself might be used as a unique id.  However, in many scenarios, `Context` contains more than the unique id itself or the unique id might
+ be retrieved as a mapping from the `Context`.  squbs allows different options to provide a unique id:
+
+   * `Context` itself is a type that can be used as a unique id, e.g., `Int`, `Long`, `java.util.UUID`
+   * `Context` extends `UniqueId.Provider` and implements `def uniqueId`
+   * `Context` is wrapped with `UniqueId.Envelope`
+   * `Context` is mapped to a unique id by calling a function
+
+With the first three options, a unique id can be retrieved directly through the context.
+
+For the last option, `RetryBidi` allows a function to be passed in as a parameter.
+
+###### Scala
+
+The following API can be used to pass a uniqueId mapper:
+
+```scala
+RetryBidi[In, Out, Context](maxRetries: Int, uniqueIdMapper: Context => Option[Any])
+```
+
+This `BidiFlow` can be joined with any flow that takes in a `(In, Context)` and outputs a `(Out, Context)`.
+
+```scala
+case class MyContext(s: String, uuid: UUID)
+
+val retryBidi = RetryBidi[String, String, MyContext](maxRetries = 2, (context: MyContext) => Some(context.uuid))
+val flow = Flow[(String, MyContext)].mapAsyncUnordered(10) { elem =>
+  (ref ? elem).mapTo[(String, MyContext)]
+}
+
+Source("a" :: "b" :: "c" :: Nil)
+  .map( _ -> MyContext("dummy", UUID.randomUUID))
+  .via(retryBidi.join(flow))
+  .runWith(Sink.seq)
+```
+
+###### Java
+
+The following API is used to pass a uniqueId mapper:
+
+```java
+public class RetryBidi {
+    public static <In, Out, Context> BidiFlow<Pair<In, Context>,
+                                              Pair<In, Context>,
+                                              Pair<Try<Out>, Context>,
+                                              Pair<Try<Out>, Context>,
+                                              NotUsed>
+    create(Long maxRetries, Function<Context, Optional<Object>> uniqueIdMapper);
+}
+```
+
+This `BidiFlow` can be joined with any flow that takes in a `akka.japi.Pair<In, Context>` and outputs a `akka.japi.Pair<Try<Out>, Context>`.
+
+```java
+class MyContext {
+    private String s;
+    private UUID uuid;
+
+    public MyContext(String s, UUID uuid) {
+        this.s = s;
+        this.uuid = uuid;
+    }
+
+    public UUID uuid() {
+        return uuid;
+    }
+};
+
+final BidiFlow<Pair<String, MyContext>, Pair<String, MyContext>,
+               Pair<Try<String>, MyContext>, Pair<Try<String>, MyContext>, NotUsed>
+                retryBidi = RetryBidi.create(maxRetries, context -> Optional.of(context.uuid));
+
+final Flow<Pair<String, MyContext>, Pair<String, MyContext>, NotUsed> flow =
+        Flow.<Pair<String, MyContext>>create()
+                .mapAsyncUnordered(10, elem -> ask(ref, elem, 5000))
+                .map(elem -> (Pair<String, MyContext>)elem);
+
+Source.from(Arrays.asList("a", "b", "c"))
+        .map(s -> new Pair<>(s, new MyContext("dummy", UUID.randomUUID())))
+        .via(retryBidi.join(flow))
+        .runWith(Sink.seq(), mat);
+
+```
+
+#### Failure decider
+
+By default, any `Failure` from the joined `Flow` is considered a failure for retry purposes.  However, the `Retry` stage also accepts an optional
+ `failureDecider` parameter to more finely control what elements from the joined `Flow` should actually be treated as failures that should be retried.
+
+##### Scala
+
+A function of type `Try[Out] => Boolean` can be provided via the `failureDecider` parameter. Below is an example where, along with any `Failure` message,
+Any response with failing http status code is also considered a failure:
+
+```scala
+
+val failureDecider = (tryResponse: Try[HttpResponse]) => tryResponse.isFailure || tryResponse.get().status().isFailure()
+
+val retryBidi = RetryBidi[Request, Response, MyContext](maxRetries = 3, (context: MyContext) => Some(context.uuid), failureDecider = Option(failureDecider), OverflowStrategy.backpressure())
+
+```
+
+##### Java
+
+A `Function<Try<Out>, Boolean>` can be provided via Retry Optional `failureDecider` parameter to create method.  Below is an example where,
+along with any `Failure` message, a `Success` of `HttpResponse` with status code `400` and above is also considered a failure:
+
+```java
+
+final Function<Try<HttpResponse>, Optional<Object>> failureDecider =
+tryResponse -> tryResponse.isFailure() || tryHttpResponse.get().status().isFailure());
+
+final BidiFlow<Pair<HttpResponse, MyContext>, Pair<HttpResponse, MyContext>, Pair<Try<HttpResponse>, MyContext>,
+    Pair<Try<HttpResponse>, MyContext>, NotUsed> retryFlow =
+    RetryBidi.create(3L, Optional.of(failureDecider), OverflowStrategy.backpressure());
+
+```
---
+++ squbs-ext/src/test/scala/org/squbs/streams/RetryBidiSpec.scala
@@ -0,0 +1,409 @@
+/*
+ * Copyright 2017 PayPal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.squbs.streams
+
+import java.util.concurrent.atomic.AtomicLong
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.Attributes.inputBuffer
+import akka.stream.{ActorMaterializer, BufferOverflowException, OverflowStrategy}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.testkit.scaladsl.{TestSink, TestSource}
+import akka.testkit.TestKit
+import org.scalatest.{AsyncFlatSpecLike, Matchers}
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.{Failure, Success, Try}
+
+class RetryBidiSpec extends TestKit(ActorSystem("RetryBidiSpec")) with AsyncFlatSpecLike with Matchers {
+
+  implicit val materializer = ActorMaterializer()
+  val failure = Failure(new Exception("failed"))
+
+  it should "require failure retryCount > 0" in {
+    an[IllegalArgumentException] should be thrownBy
+      RetryBidi[String, String, NotUsed](-1)
+  }
+
+  it should "return all expected elements if no failures" in {
+    val flow = Flow[(String, Long)].map {
+      case (elem, ctx) => (Success(elem), ctx)
+    }
+    val retryBidi = RetryBidi[String, String, Long](1)
+
+    var context = 0L
+    val result = Source("a" :: "b" :: "c" :: Nil)
+      .map { s => context += 1; (s, context) }
+      .via(retryBidi.join(flow))
+      .runWith(Sink.seq)
+
+    val expected = (Success("a"), 1) :: (Success("b"), 2) :: (Success("c"), 3) :: Nil
+    result map {
+      _ should contain theSameElementsAs expected
+    }
+  }
+
+  it should "emit when first fail retries are exhausted" in {
+    val flow = Flow[(String, Long)].map {
+      case ("a", ctx) => (failure, ctx)
+      case (elem, ctx) => (Success(elem), ctx)
+    }
+    var context = 0L
+    val result = Source("a" :: "b" :: "c" :: Nil)
+      .map { e => context += 1; (e, context) }
+      .via(RetryBidi[String, String, Long](3).join(flow))
+      .runWith(Sink.seq)
+
+    val expected = (Success("b"), 2) :: (Success("c"), 3) :: (failure, 1) :: Nil
+    result map {
+      _ should contain theSameElementsAs expected
+    }
+  }
+
+  it should "return failure if middle element failure exhausts retries" in {
+    val flow = Flow[(String, Long)].map {
+      case ("e", ctx) => (failure, ctx)
+      case (elem, ctx) => (Success(elem), ctx)
+    }
+
+    val retryBidi = RetryBidi[String, String, Long](3)
+
+    var context = 0L
+    val result = Source("d" :: "e" :: "f" :: Nil)
+      .map { s => context += 1; (s, context) }
+      .via(retryBidi.join(flow))
+      .runWith(Sink.seq)
+
+    val expected = (Success("d"), 1) :: (failure, 2) :: (Success("f"), 3) :: Nil
+    result map {
+      _ should contain theSameElementsAs expected
+    }
+  }
+
+  it should "return failure if last failure exhausts retries" in {
+    val flow = Flow[(String, Long)].map {
+      case ("f", ctx) => (failure, ctx)
+      case (elem, ctx) => (Success(elem), ctx)
+    }
+
+    val retryBidi = RetryBidi[String, String, Long](1)
+
+    var context = 0L
+    val result = Source("d" :: "e" :: "f" :: Nil)
+      .map { s => context += 1; (s, context) }
+      .via(retryBidi.join(flow))
+      .runWith(Sink.seq)
+
+    val expected = (Success("d"), 1) :: (Success("e"), 2) :: (failure, 3) :: Nil
+    result map {
+      _ should contain theSameElementsAs expected
+    }
+  }
+
+  it should "return exhausted failures in expected FIFO order" in {
+    val flow = Flow[(String, Long)].map {
+      case ("f", ctx) => (failure, ctx)
+      case ("e", ctx) => (failure, ctx)
+      case (elem, ctx) => (Success(elem), ctx)
+    }
+
+    val retryBidi = RetryBidi[String, String, Long](1)
+
+    var context = 0L
+    val result = Source("d" :: "e" :: "f" :: Nil)
+      .map { s => context += 1; (s, context) }
+      .via(retryBidi.join(flow))
+      .runWith(Sink.seq)
+
+    val expected = (Success("d"), 1) :: (failure, 2) :: (failure, 3) :: Nil
+    result map {
+      _ should contain theSameElementsInOrderAs expected
+    }
+  }
+
+  it should "perform the correct number of retries" in {
+    val count = new AtomicLong(0)
+    val flow = Flow[(String, Long)].map {
+      case (_, ctx) => count.getAndIncrement(); (failure, ctx)
+    }
+
+    val maxRetry = 10L
+    val retryBidi = RetryBidi[String, String, Long](maxRetry)
+
+    val context = 42L
+    val result = Source("x" :: Nil)
+      .map { s => (s, context) }
+      .via(retryBidi.join(flow))
+      .runWith(Sink.seq)
+
+    result map { r =>
+      r should contain theSameElementsAs (failure, 42) :: Nil
+      count.get shouldEqual maxRetry + 1
+    }
+  }
+
+  it should "return Success when a failure is retried successfully" in {
+    var first = true
+    val flow = Flow[(String, Long)].map {
+      case ("y", ctx) if first =>
+        first = false
+        (failure, ctx)
+      case (elem, ctx) => (Success(elem), ctx)
+    }
+
+    val retryBidi = RetryBidi[String, String, Long](2)
+
+    var context = 0L
+    val result = Source("x" :: "y" :: "z" :: Nil)
+      .map { s => context += 1; (s, context) }
+      .via(retryBidi.join(flow))
+      .runWith(Sink.seq)
+
+    val expected = (Success("x"), 1) :: (Success("z"), 3) :: (Success("y"), 2) :: Nil
+    result map {
+      _ should contain theSameElementsAs expected
+    }
+  }
+
+  it should "allow a uniqueid mapper via UniqueId.Provider" in {
+
+    case class MyContext(id: Long) extends UniqueId.Provider {
+      override def uniqueId: Any = id
+    }
+
+    val flow = Flow[(String, MyContext)].map {
+      case (elem, ctx) => (Success(elem), ctx)
+    }
+    val retry = RetryBidi[String, String, MyContext](2, uniqueIdMapper = (context: MyContext) => Some(context.uniqueId))
+
+    var counter = 0L
+    val result = Source("a" :: "b" :: "c" :: Nil)
+      .map { s => counter += 1; (s, MyContext(counter)) }
+      .via(retry.join(flow))
+      .map { case (s, _) => s }
+      .runWith(Sink.seq)
+
+    val expected = Success("a") :: Success("b") :: Success("c") :: Nil
+    result map {
+      _ should contain theSameElementsAs expected
+    }
+  }
+
+  it should "cancel upstream if downstream cancels" in {
+    val bottom = Flow[(String, Long)].map {
+      case (_, ctx) => (failure, ctx)
+    }
+    val retry = RetryBidi[String, String, Long](5)
+    var context = 0L
+    val (source, sink) = TestSource.probe[String]
+      .map { s => context += 1; (s, context) }
+      .via(retry.join(bottom))
+      .toMat(TestSink.probe)(Keep.both).run()
+
+    sink.request(2)
+    source.sendNext("a")
+    sink.cancel()
+    source.expectCancellation()
+    succeed
+  }
+
+  it should "keep retrying after upstream completes" in {
+    val bottom = Flow[(String, Long)].map {
+      case (_, ctx) => (failure, ctx)
+    }
+    val retry = RetryBidi[String, String, Long](5)
+    var context = 0L
+    val (source, sink) = TestSource.probe[String]
+      .map { s => context += 1; (s, context) }
+      .via(retry.join(bottom))
+      .toMat(TestSink.probe)(Keep.both).run()
+
+    sink.request(1)
+    source.sendNext("a").sendNext("b").sendComplete()
+    val next = sink.requestNext(3 seconds)
+    assert((failure, 1) == next)
+  }
+
+  it should "decide on failures based on the provided function" in {
+    val bottom = Flow[(String, Long)].map {
+      case (elem, ctx) => (Success(elem), ctx)
+    }
+    val failureDecider = (out: Try[String]) => out.isFailure || out.equals(Success("a")) // treat "a" as a failure for retry
+    val retry = RetryBidi[String, String, Long](2, failureDecider = Option(failureDecider))
+
+    var context = 0L
+    val (source, sink) = TestSource.probe[String]
+      .map { s => context += 1; (s, context) }
+      .via(retry.join(bottom))
+      .toMat(TestSink.probe)(Keep.both).run()
+
+    source.sendNext("a").sendNext("b")
+    val nextA = sink.request(1).requestNext()
+    assert((Success("a"), 1) == nextA)
+  }
+
+  it should "drain all elements when upstream finishes" in {
+    val bottom = Flow[(String, Long)].map {
+      case ("1", ctx) => (Success("1"), ctx)
+      case (_, ctx) => (failure, ctx)
+    }
+    val retry = RetryBidi[String, String, Long](10)
+
+    val (source, sink) = TestSource.probe[String]
+      .map(x => (x.toString, x.toLong))
+      .via(retry.join(bottom))
+      .toMat(TestSink.probe)(Keep.both).run()
+
+    source.sendNext("1").sendNext("2").sendNext("3").sendComplete()
+    sink.request(3).expectNext((Success("1"), 1L)).expectNextUnordered((failure, 2L), (failure, 3L))
+    succeed
+  }
+
+  it should "drop head elements and emit them when dropHead buffer mode" in {
+    val bottom = Flow[(String, Long)].delay(10.millis).map {
+      case (_, ctx) => (failure, ctx)
+    }
+    val retry = RetryBidi[String, String, Long](10, overflowStrategy = OverflowStrategy.dropHead)
+      .withAttributes(inputBuffer(initial = 1, max = 3))
+
+    val sink = Source (1 to 5)
+      .map(x => (x.toString, x.toLong))
+      .via(retry.join(bottom))
+      .runWith(TestSink.probe)
+
+    // 1 and 2 element are emitted after being dropped from buffer, 3-5 after exhausting all retries
+    sink
+      .request(5)
+      .expectNoMsg(10.millis)
+      .expectNext((failure, 1L), (failure, 2L))
+      .expectNoMsg(100.millis)
+      .expectNext((failure, 3L), (failure, 4L), (failure, 5L))
+    succeed
+  }
+
+  it should "drop tail elements and emit them when dropTail buffer mode" in {
+    val bottom = Flow[(String, Long)].delay(10.millis).map {
+      case (_, ctx) => (failure, ctx)
+    }
+    val retry = RetryBidi[String, String, Long](10, overflowStrategy = OverflowStrategy.dropTail)
+      .withAttributes(inputBuffer(initial = 1, max = 3))
+
+    val sink = Source(1 to 5)
+      .map(x => (x.toString, x.toLong))
+      .via(retry.join(bottom))
+      .runWith(TestSink.probe)
+
+    // element 3 and 4 are emitted after being dropped from buffer
+    sink.request(5)
+      .expectNoMsg(10.millis)
+      .expectNext((failure, 3L), (failure, 4L))
+      .expectNoMsg(100.millis)
+      .expectNext((failure, 1L), (failure, 2L), (failure, 5L))
+    succeed
+  }
+
+  it should "drop new elements when dropNew buffer mode" in {
+    val bottom = Flow[(String, Long)].delay(10.millis).map {
+      case (_, ctx) => (failure, ctx)
+    }
+    val retry = RetryBidi[String, String, Long](2, overflowStrategy = OverflowStrategy.dropNew)
+      .withAttributes(inputBuffer(initial = 1, max = 3))
+
+    val sink = Source(1 to 5)
+      .map(x => (x.toString, x.toLong))
+      .via(retry.join(bottom))
+      .runWith(TestSink.probe)
+
+    sink.request(5)
+      .expectNext((failure, 1L), (failure, 2L),(failure, 3L))
+      .expectComplete() // element 4 and 5 are dropped on buffer full
+    succeed
+  }
+
+  it should "drop all elements in buffer when dropBuffer mode" in {
+    val bottom = Flow[(String, Long)].delay(10.millis).map {
+      case (_, ctx) => (failure, ctx)
+    }
+    val retry = RetryBidi[String, String, Long](10, overflowStrategy = OverflowStrategy.dropBuffer)
+      .withAttributes(inputBuffer(initial = 1, max = 4))
+
+    val sink = Source(1 to 8)
+      .map(x => (x.toString, x.toLong))
+      .via(retry.join(bottom))
+      .runWith(TestSink.probe)
+
+    sink.request(8) // first 16 elements are dropped and immitted when buffer is full the rest after exhausting retries
+      .expectNoMsg(10.millis)
+      .expectNext((failure, 1L), (failure, 2L), (failure, 3L), (failure, 4L))
+      .expectNoMsg(100.millis)
+      .expectNext((failure, 5L), (failure, 6L), (failure, 7L), (failure, 8L))
+    succeed
+  }
+
+  it should "backpressures upstream when buffer full" in {
+    val bottom = Flow[(String, Long)].delay(10.millis).map {
+      case (_, ctx) => (failure, ctx)
+    }
+    val retry = RetryBidi[String, String, Long](1).withAttributes(inputBuffer(initial = 1, max = 1))
+
+    val sink = Source(1 to 3)
+      .map(x => (x.toString, x.toLong))
+      .via(retry.join(bottom))
+      .runWith(TestSink.probe)
+
+    sink.request(3)
+      .expectNoMsg(10.millis)
+      .expectNext((failure, 1L))
+      .expectNoMsg(10.millis)
+    succeed
+  }
+
+  it should "fail when buffer full on fail mode" in {
+    val bottom = Flow[(String, Long)].delay(10.millis).map {
+      case (_, ctx) => (failure, ctx)
+    }
+    val retry = RetryBidi[String, String, Long](1, overflowStrategy = OverflowStrategy.fail)
+      .withAttributes(inputBuffer(initial = 1, max = 1))
+
+    val sink = Source(1 to 3)
+      .map(x => (x.toString, x.toLong))
+      .via(retry.join(bottom))
+      .runWith(TestSink.probe)
+
+    sink.request(3).expectError(new BufferOverflowException("Retry buffer overflow for retry stage (max capacity was: 1)!"))
+    succeed
+  }
+
+  it should "retry with a large data set" in {
+    val bottom = Flow[(Long, Long)].map {
+      case (elem, ctx) => if (ctx % 7 == 0) (failure, ctx) else (Success(elem), ctx) // fail every 7'th element
+    }
+    val retry = RetryBidi[Long, Long, Long](1)
+
+    val result = Source(1L to 10000)
+      .map(x => (x, x))
+      .via(retry.join(bottom))
+      .runWith(Sink.seq)
+
+    val expected = 1 to 10000 map (x => if (x % 7 == 0) (failure, x) else (Success(x), x))
+    result map {
+      _ should contain theSameElementsAs expected
+    }
+  }
+}
