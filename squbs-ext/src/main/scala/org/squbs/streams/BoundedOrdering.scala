e76cbcbb99a03a83008c5cefe5f89dee962ad363
Akara Sucharitakul <asucharitakul@paypal.com>
2018-02-04 16:47:34 -0800

    Re-worked OrderingStage into BoundedOrdering (Fixes #557, #562, #563)

---
+++ squbs-ext/src/main/scala/org/squbs/streams/BoundedOrdering.scala
@@ -0,0 +1,186 @@
+/*
+ *  Copyright 2018 PayPal
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.squbs.streams
+
+import java.util.Comparator
+import java.util.function.{Function => JFunc}
+
+import akka.NotUsed
+import akka.stream.javadsl.{Flow => JFlow}
+import akka.stream.scaladsl.Flow
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+import scala.collection.mutable
+
+object BoundedOrdering {
+
+  /**
+   * Scala API
+   *
+   * Creates a [[Flow]] representing an [[BoundedOrdering]]. Orders the stream within a sliding window of n
+   * elements specified by the maxBounded parameter. Each element passing through the [[BoundedOrdering]] has
+   * an id. The elements can be ordered by the ordering function and the next expected id can be derived from
+   * the current id.
+   *
+   * @param maxBounded    The number of elements to wait for an out-of-order element.
+   * @param initialId The id of the first element getting to the [[BoundedOrdering]].
+   * @param nextId    Function to calculate the subsequent id from the current id.
+   * @param getId     Function to get the id from an element.
+   * @param ordering  Defines how the element ids are being ordered.
+   * @tparam A The input and output type of this stream component.
+   * @tparam B The type of the element's id.
+   * @return The [[Flow]] representing the given [[BoundedOrdering]]
+   */
+  def apply[A, B](maxBounded: Int, initialId: B, nextId: B => B, getId: A => B)(implicit ordering: Ordering[B]):
+  Flow[A, A, NotUsed] =
+    Flow.fromGraph(new BoundedOrdering(maxBounded, initialId, nextId, getId))
+
+  /**
+   * Java API
+   *
+   * Creates a [[Flow]] representing an [[BoundedOrdering]] using the natural ordering of the element id.
+   * Orders the stream within a sliding window of n elements specified by the maxBounded parameter.
+   * Each element passing through the [[BoundedOrdering]] has an id. The elements can be ordered by comparing the id
+   * and the next expected id can be derived from the current id.
+   *
+   * @param maxBounded    The number of elements to wait for an out-of-order element.
+   * @param initialId The id of the first element getting to the [[BoundedOrdering]].
+   * @param nextId    Function to calculate the subsequent id from the current id.
+   * @param getId     Function to get the id from an element.
+   * @tparam A The input and output type of this stream component.
+   * @tparam B The type of the element's id.
+   * @return The [[Flow]] representing the given [[BoundedOrdering]]
+   */
+  def create[A, B <: Comparable[B]](maxBounded: Int, initialId: B, nextId: JFunc[B, B], getId: JFunc[A, B]):
+  JFlow[A, A, NotUsed] =
+    JFlow.fromGraph(new BoundedOrdering(maxBounded, initialId, nextId.apply, getId.apply)
+    ((x: B, y: B) => x.compareTo(y)))
+
+  /**
+   * Java API
+   *
+   * Creates a [[Flow]] representing an [[BoundedOrdering]]. Orders the stream within a sliding window of n
+   * elements specified by the maxBounded parameter. Each element passing through the [[BoundedOrdering]] has
+   * an id. The elements can be ordered by the id comparator and the next expected id can be derived from the
+   * current id.
+   *
+   * @param maxBounded     The number of elements to wait for an out-of-order element.
+   * @param initialId  The id of the first element getting to the [[BoundedOrdering]].
+   * @param nextId     Function to calculate the subsequent id from the current id.
+   * @param getId      Function to get the id from an element.
+   * @param comparator The comparator used to find the ordering of the elements' ids.
+   * @tparam A The input and output type of this stream component.
+   * @tparam B The type of the element's id.
+   * @return The [[Flow]] representing the given [[BoundedOrdering]]
+   */
+  def create[A, B](maxBounded: Int, initialId: B, nextId: JFunc[B, B], getId: JFunc[A, B], comparator: Comparator[B]):
+  JFlow[A, A, NotUsed] =
+    JFlow.fromGraph(new BoundedOrdering[A, B](maxBounded, initialId, nextId.apply, getId.apply)
+    (Ordering.comparatorToOrdering(comparator)))
+}
+
+/**
+ * Orders the stream within a sliding window of [[maxBounded]] elements. Each element passing through the
+ * [[BoundedOrdering]] has an id. The elements can be ordered by the ordering function and the next expected
+ * id can be derived from the current id.
+ *
+ * It takes an element and emits downstream if the [[getId(elem)]] matches the currently stored id, if not enqueues to
+ * a priority queue. When an element is emitted, the stored id is updated by calling [[nextId]] to match the next
+ * expected element and an element is dequeued from the priority queue and emitted if it matches the updated id.
+ * If the out-of-order element arrives at a later point than [[maxBounded]] then it is emitted straight downstream.
+ *
+ * @param maxBounded    The number of elements to wait for an out-of-order element.
+ * @param initialId The id of the first element getting to the [[BoundedOrdering]].
+ * @param nextId    Function to calculate the subsequent id from the current id.
+ * @param getId     Function to get the id from an element.
+ * @param ordering  Defines how the element ids are being ordered.
+ * @tparam A The input and output type of this stream component.
+ * @tparam B The type of the element's id.
+ *
+ * '''Emits when''' [[getId(elem)]] matches the current state, if not enqueues it to priority queue
+ *
+ * '''Backpressures when''' downstream backpressures
+ *
+ * '''Completes when''' upstream completes
+ *
+ * '''Cancels when''' downstream cancels
+ */
+class BoundedOrdering[A, B](maxBounded: Int, initialId: B, nextId: B => B, getId: A => B)
+                           (implicit val ordering: Ordering[B])
+  extends GraphStage[FlowShape[A, A]] {
+
+  require(maxBounded > 0)
+
+  val in: Inlet[A] = Inlet("Filter.in")
+  val out: Outlet[A] = Outlet("Filter.out")
+  val shape: FlowShape[A, A] = FlowShape.of(in, out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+
+      var expectedId: B = initialId
+
+      implicit val elementPriority: Ordering[A] = Ordering.by(getId).reverse
+      val pq: mutable.PriorityQueue[A] = mutable.PriorityQueue.empty
+
+      @tailrec
+      def elemsToPush(q: Queue[A], nextExpectedId: B): (Queue[A], B) =
+        pq.headOption match {
+          case Some(e) if nextExpectedId == getId(e) =>
+            pq.dequeue()
+            elemsToPush(q :+ e, nextId(nextExpectedId))
+          case _ => (q, nextExpectedId)
+        }
+
+      def outOfBand(element: A): Boolean = ordering.lt(getId(element), expectedId)
+
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+
+          val elem = grab[A](in)
+
+          // 1. Element id matches expected id, send it down and unwind pq as long as element ids match expected.
+          // 2. Element is out-of-band and missed buffer, should have arrived long ago, just send it down.
+          // 3. Buffer full, need to flush elements downstream. Potentially causes out-of-band later.
+          // 4. Element id does not match expected id, queue up in priority queue.
+
+          if(expectedId == getId(elem)) {
+            val (elems, nextExpectedId) = elemsToPush(Queue(elem), nextId(expectedId))
+            expectedId = nextExpectedId
+            emitMultiple(out, elems)
+          } else if (outOfBand(elem)) {
+            emit(out, elem)
+          } else if (pq.size >= maxBounded) {
+            pq += elem
+            val (elems, nextExpectedId) = elemsToPush(Queue.empty, getId(pq.head))
+            expectedId = nextExpectedId
+            emitMultiple(out, elems)
+          } else {
+            pq += elem
+            tryPull(in)
+          }
+        }
+      })
+
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = tryPull(in)
+      })
+    }
+}
---
+++ squbs-ext/src/test/scala/org/squbs/streams/BoundedOrderingSpec.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2018 PayPal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.squbs.streams
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{FlatSpec, Matchers}
+
+class BoundedOrderingSpec extends FlatSpec with Matchers with ScalaFutures {
+
+  implicit val system = ActorSystem("OrderingStateSpec")
+  implicit val mat = ActorMaterializer()
+
+  it should "require waitFor > 0" in {
+    an [IllegalArgumentException] should be thrownBy BoundedOrdering[Int, Int](maxBounded = 0, 1, _ + 1, identity)
+  }
+
+  it should "retain order of a stream" in {
+    val boundedOrdering = BoundedOrdering[Int, Int](maxBounded = 5, 1, _ + 1, identity)
+    val input = List(1, 2, 3, 4, 5)
+    val output = Source(input).via(boundedOrdering).toMat(Sink.seq)(Keep.right).run()
+    output.futureValue should contain theSameElementsInOrderAs input
+  }
+
+  it should "re-order the stream completely within the ordering range" in {
+    val boundedOrdering = BoundedOrdering[Int, Int](maxBounded = 5, 1, _ + 1, identity)
+    val input = List(2, 3, 4, 1, 5, 7, 8, 6, 9, 10)
+    val output = Source(input).via(boundedOrdering).toMat(Sink.seq)(Keep.right).run()
+    output.futureValue should contain theSameElementsInOrderAs input.sorted
+  }
+
+  it should "re-order the stream incompletely outside of the ordering range" in {
+    val boundedOrdering = BoundedOrdering[Int, Int](maxBounded = 5, 1, _ + 1, identity)
+    val input = List(1, 3, 4, 5, 6, 7, 8, 9, 2, 10)
+    val output = Source(input).via(boundedOrdering).toMat(Sink.seq)(Keep.right).run()
+    output.futureValue should contain theSameElementsInOrderAs input
+  }
+
+  it should "ignore the missing element and keep the stream moving" in {
+    val boundedOrdering = BoundedOrdering[Int, Int](maxBounded = 5, 1, _ + 1, identity)
+    val input = List(1, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+    val output = Source(input).via(boundedOrdering).toMat(Sink.seq)(Keep.right).run()
+    output.futureValue should contain theSameElementsInOrderAs input
+  }
+
+  it should "re-order the stream with identifier type different from message type" in {
+    case class Element(id: Long, content: String)
+    val boundedOrdering = BoundedOrdering[Element, Long](maxBounded = 5, 1L, _ + 1L, _.id)
+    val input = List(Element(1, "one"), Element(3, "three"), Element(5, "five"), Element(2, "two"), Element(6, "six"),
+      Element(7, "seven"), Element(8, "eight"), Element(9, "nine"), Element(10, "ten"), Element(4, "four"))
+    val wisb = List(Element(1, "one"), Element(2, "two"), Element(3, "three"), Element(5, "five"), Element(6, "six"),
+      Element(7, "seven"), Element(8, "eight"), Element(9, "nine"), Element(10, "ten"), Element(4, "four"))
+
+    val output = Source(input).via(boundedOrdering).toMat(Sink.seq)(Keep.right).run()
+    output.futureValue should contain theSameElementsInOrderAs wisb
+  }
+
+  it should "re-order the stream using custom id ordering" in {
+    case class Element(id: String, content: String)
+    implicit val order: Ordering[String] = Ordering.by(_.toInt)
+    val boundedOrdering = BoundedOrdering[Element, String](maxBounded = 5, "2", s => (s.toInt + 2).toString, _.id)
+    val input = List(Element("2", "one"), Element("6", "three"), Element("10", "five"), Element("4", "two"),
+      Element("12", "six"), Element("14", "seven"), Element("16", "eight"), Element("18", "nine"),
+      Element("20", "ten"), Element("8", "four"))
+    val wisb = List(Element("2", "one"), Element("4", "two"), Element("6", "three"), Element("10", "five"),
+      Element("12", "six"), Element("14", "seven"), Element("16", "eight"), Element("18", "nine"),
+      Element("20", "ten"), Element("8", "four"))
+
+    val output = Source(input).via(boundedOrdering).toMat(Sink.seq)(Keep.right).run()
+    output.futureValue should contain theSameElementsInOrderAs wisb
+  }
+}
